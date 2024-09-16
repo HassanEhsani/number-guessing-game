bc9fdbe155b964e4aaac4eedd9453a90be861c7c
HassanEhsani <hassan.ehsani.am@gmail.com>
2024-09-16 16:29:03 +0300

    wrote the code about the number when the user try it to guess the number

--- app.py
+++ app.py
@@ -29,7 +29,10 @@ while True:
         print("You got it!")
         break
     else:
-        print("You got a wrong!")
+       if user_guess > random_number:
+           print("You were above the number!")
+       else:
+           print("You were below the number!")
         
 print("You got it in", guesses, "guesses")
   
